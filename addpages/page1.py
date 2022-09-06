b06baa07e8649f95f89e4b9efcc40cd856ef2622
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-09-07 04:41:34 +0900

    Update page1.py

--- addpages/page1.py
+++ addpages/page1.py
@@ -1,7 +1,16 @@
 import streamlit as st
 
 from add_pages import pages
-from db.app_1 import ch
+from db.app_1 import crte
+from db.app_1 import preed
+from db.app_1 import act
+from db.app_1 import stre
+from db.app_1 import seab
+from db.app_1 import pd
+from db.app_1 import stfor
+from db.app_1 import imff
+from db.app_1 import frez
+from db.app_1 import mo
 def app():
 
     
@@ -14,17 +23,17 @@ def app():
 
     app.add_page('선택하세요',ch.app)
     app.add_page('명령어', app_1/rd.py.app)
-    app.add_page('conda create -n test1 python=3.7.4 ipython numpy matplotlib pands scipy scikit-learn tensorflow keras ', app_1/crte.app)
-    app.add_page('proceed','db/app_1/preed.app')
-    app.add_page('conda activate test1', 'db/app_1/act.app')
-    app.add_page('pip install streamlit', 'db/app_1/stre.app')
-    app.add_page('pip install seaborn', 'db/app_1/seab.app')
-    app.add_page('pip install pandas', 'db/app_1/pd.app')
-    app.add_page('pip install streamlit_forum', 'db/app_1/stfor.app')
-    app.add_page('pip install imageio_ffmpeg', 'db/app_1/imff.app')
+    app.add_page('conda create -n test1 python=3.7.4 ipython numpy matplotlib pands scipy scikit-learn tensorflow keras ', crte.app)
+    app.add_page('proceed',preed.app)
+    app.add_page('conda activate test1', act.app)
+    app.add_page('pip install streamlit', stre.app)
+    app.add_page('pip install seaborn', seab.app)
+    app.add_page('pip install pandas', pd.app)
+    app.add_page('pip install streamlit_forum', stfor.app)
+    app.add_page('pip install imageio_ffmpeg', imff.app)
 
-    app.add_page('pip freeze', 'db/app_1/frez.app')
-    app.add_page('more', 'db/app_1/mo.app')
+    app.add_page('pip freeze', frez.app)
+    app.add_page('more', mo.app)
 
 
     app.run()
