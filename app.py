2b7a69151dfd89a6c9b81083a93368c1a460b97c
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-09-07 02:18:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import streamlit as st
 #import app_3
 st.title('프로젝트1')
 
-app = python_prj_1/add_pagespy/pages()
+app = add_pages.py/pages()
 app.add_page('선텍하세요',db/app_1/app_0.app)
-app.add_page('개발환경 설정',db/app_1/app_1.app )
+#app.add_page('개발환경 설정',db/app_1/app_1.app )
 #app.add_page('자료표출',db/app_1/app_2.app)
 #app.add_page('시계열자료',db/app_1/app_3.app)
 
