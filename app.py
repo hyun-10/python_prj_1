6426aef13b3f3c6d9d4b407f9be6d9ef803efb5e
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-09-07 02:29:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.title('프로젝트1')
 
 
 #app.addpages('선텍하세요',db/app_1/app_0.app)
-app.add_pages('개발환경 설정',page1.app )
+app.add_page('개발환경 설정',page1.app )
 #app.add_page('자료표출',db/app_1/app_2.app)
 #app.add_page('시계열자료',db/app_1/app_3.app)
 
