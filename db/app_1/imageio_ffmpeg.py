1ea1076958883576bfd69a932bd87bd0434daa5a
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-09-07 04:48:11 +0900

    Add files via upload

---
+++ db/app_1/imageio_ffmpeg.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import streamlit as st
+def app():
+	st.write("Collecting imageio_ffmpeg\
+  Using cached imageio_ffmpeg-0.4.7-py3-none-win_amd64.whl (22.6 MB)\
+Installing collected packages: imageio-ffmpeg\
+Successfully installed imageio-ffmpeg-0.4.7")
