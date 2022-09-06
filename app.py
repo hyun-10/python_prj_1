de3ed7206e7c4c88aab2b83bf37da66ce2ec8848
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-09-07 02:12:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import streamlit as st
 
-from multiple.multi_pages import multipage
-import app_0
-import app_1
+
+
 #import app_2
 #import app_3
 st.title('프로젝트1')
 
 app = multipage()
-app.add_page('선텍하세요',app_0.app)
-app.add_page('개발환경 설정',app_1.app )
-#app.add_page('자료표출',app_2.app)
-#app.add_page('시계열자료',app_3.app)
+app.add_page('선텍하세요',db/app_1/app_0.app)
+app.add_page('개발환경 설정',db/app_1/app_1.app )
+#app.add_page('자료표출',db/app_1/app_2.app)
+#app.add_page('시계열자료',db/app_1/app_3.app)
 
 
 app.run()
