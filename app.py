ff3c13f6ddadaf5c9cc073d6c5201088b58e5c9e
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-09-07 04:52:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ import streamlit as st
 
 from add_pages import pages
 from addpages import page1 as p1
+from addpages import page1 as p2
+from addpages import page1 as p3
+
 app = pages()
 
 
 
 
 app.add_page('개발환경 설정',p1.app )
-
+app.add_page('자료표출',p2.app)
+app.add_page('시계열자료',p3.app)
 
 app.run()
 
